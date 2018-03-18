992412f4db9cf959baf70d8a550107dda29b5291
ararage_kun <prz.perez.ric@gmail.com>
2018-03-18 15:29:56 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    //SideNav
+    $('.button-collapse').sideNav();
+    //Slider
+    $('.slider').slider({
+        indicators:false,
+        height:500,
+        transition:500,
+        interval:6000
+    })
+})
